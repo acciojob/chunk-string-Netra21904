8fca367f51073d31747cd8cb006af85766cf9eb7
Netra21904 <128806086+Netra21904@users.noreply.github.com>
2025-02-06 13:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
-  // your code here
+  if (str === null || str === undefined) {  // Handle null or undefined input
+    return [];
+  }
+
+  if (typeof size !== 'number' || size <= 0) { //Handle invalid chunk size
+    return [];
+  }
+
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.substring(i, i + size));
+  }
+  return result;
 }
 
 // Do not change the code below
